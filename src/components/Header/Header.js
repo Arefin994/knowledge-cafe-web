0b6cfe76e8bbc50fc8cdf4e5759ce82d4aa6cc9a
Arefin994 <arefinamin994@gmail.com>
2023-04-01 21:57:32 +0600

    Nav bar Updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,10 @@ const Header = () => {
                 <div>
                     <h2>Knowledge Cafe</h2>
                 </div>
-                <div>
+                <div className='d-flex right-nav'>
+                    <a href=""> Blogs</a>
+                    <a href=""> Authors</a>
+                    <a href="">Contact</a>
                     <img src="https://images.unsplash.com/photo-1502323777036-f29e3972d82f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" srcset="" />
                 </div>
             </nav>
